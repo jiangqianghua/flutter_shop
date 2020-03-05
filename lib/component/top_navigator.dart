feb2b7b68e27452e55f458d1895a47f876434be5
jiangqianghua <240437339@qq.com>
2020-03-06 00:54:29 +0800

    添加导航栏禁止滚动

--- lib/component/top_navigator.dart
+++ lib/component/top_navigator.dart
@@ -26,11 +26,13 @@ class TopNavigator extends StatelessWidget {
     return Container(
       height: ScreenUtil().setHeight(320),
       padding: EdgeInsets.all(3.0),
-      child: GridView.count(crossAxisCount: 5,
-      padding: EdgeInsets.all(5.0),
-      children: navigatorList.map((item){
-        return _gridViewItemUI(context, item);
-      }).toList(),
+      child: GridView.count(
+        physics: NeverScrollableScrollPhysics(), // 禁止滚动，防止和外界滚动冲突
+        crossAxisCount: 5,
+        padding: EdgeInsets.all(5.0),
+        children: navigatorList.map((item){
+          return _gridViewItemUI(context, item);
+        }).toList(),
       ),
     );
   }
