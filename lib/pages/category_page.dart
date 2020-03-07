93209a3ff0e2bdd080bfb45a4fe061db51cb443b
jiangqianghua <240437339@qq.com>
2020-03-07 23:54:08 +0800

    添加分类左侧导航

--- lib/pages/category_page.dart
+++ lib/pages/category_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../service/service_method.dart';
 import 'dart:convert';
 import '../model/category.dart';
@@ -11,17 +12,79 @@ class CategoryPage extends StatefulWidget {
 class _CategoryPageState extends State<CategoryPage> {
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("商品分类"),
+      ),
+      body: Row(
+        children: <Widget>[
+          LeftCagetoryNav()
+        ],
+      ),
+    );
+  }
+}
+// 左侧大类导航
+class LeftCagetoryNav extends StatefulWidget {
+  @override
+  _LeftCagetoryNavState createState() => _LeftCagetoryNavState();
+}
+
+class _LeftCagetoryNavState extends State<LeftCagetoryNav> {
+  List<CategoryBigModel> list = [];
+  @override
+  void initState(){
     _getCategory();
+    super.initState();
+  }
+  Widget _leftInkWellItem(int index){
+    return InkWell(
+      onTap: (){
+
+      },
+      child: Container(
+        height: ScreenUtil().setHeight(100),
+        padding: EdgeInsets.only(left:10, top:16),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          border: Border(
+            bottom: BorderSide(width: 1, color:Colors.black12)
+          )
+        ),
+        child: Text(
+          list[index].mallCategoryName,
+          style: TextStyle(
+            fontSize: ScreenUtil().setSp(28)
+          ),
+        ),
+      )
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      
+      width: ScreenUtil().setWidth(180),
+      decoration: BoxDecoration(
+        border: Border(
+          right: BorderSide(width: 1, color: Colors.black12)
+        )
+      ),
+      child: ListView.builder(itemBuilder: (context, index){
+        return _leftInkWellItem(index);
+      },itemCount: list.length,
+      ),
     );
   }
 
-  void _getCategory() async {
+    void _getCategory() async {
     await getCategory().then((val){
       var data = json.decode(val.toString());
-      CategoryBigListModel list = CategoryBigListModel.fromJson(data['data']);
-      list.data.forEach((item)=>print(item.mallCategoryName));
+      CategoryBigListModel categoryBigListModel = CategoryBigListModel.fromJson(data['data']);
+      setState(() {
+        list = categoryBigListModel.data;
+      });
+      // list = list.data.forEach((item)=>print(item.mallCategoryName));
     });
   }
 }
+
