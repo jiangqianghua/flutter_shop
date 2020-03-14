d42c40a3d18a8c44a95909936b94be435470911a
jiangqianghua <240437339@qq.com>
2020-03-15 01:00:38 +0800

    添加购物车item

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,12 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../provide/cart.dart';
+import './cart_page/cart_item.dart';
 
 class CartPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('购物车'),
+      appBar: AppBar(
+        title: Text('购物车')
       ),
+      body:FutureBuilder(
+        future: _getCartInfo(context),
+        builder: (context, snapshot){
+          if (snapshot.hasData){
+            List cartList = _getCartProvide(context).cartList;
+            return ListView.builder(
+              itemCount: cartList.length,
+              itemBuilder: (context, index){
+                return CartItem(cartList[index]);
+              }
+              );
+          } else {
+            return Center(
+              child: Text('正在加载'),
+            );
+          }
+        },
+      )
     );
+  }
+
+  Future<String> _getCartInfo(BuildContext context) async{
+    await _getCartProvide(context).getCartInfo();
+    return "end";
+  }
+
+  CartProvide _getCartProvide(BuildContext context){
+    return Provider.of<CartProvide>(context);
   }
 }
---
+++ lib/pages/cart_page/cart_item.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_shop/model/cartinfo.dart';
+import 'package:provider/provider.dart';
+import '../../provide/cart.dart';
+import '../../model/cartinfo.dart';
+
+class CartItem extends StatelessWidget {
+  final CartInfoModel item;
+  CartItem(this.item);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.fromLTRB(5.0, 2.0, 5.0, 2.0),
+      padding: EdgeInsets.fromLTRB(5.0, 10.0, 5.0, 10.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border(
+          bottom: BorderSide(width: 1,color: Colors.black12)
+        )
+      ),
+      child: Row(
+        children: <Widget>[
+          _cartCheckBt(item),
+          _cartImage(item),
+          _cartGoodsName(item),
+          _cartPrice(item)
+        ],
+      ),
+    );
+  }
+
+  Widget _cartCheckBt(item){
+    return Container(
+      child: Checkbox(
+        value: true,
+        activeColor: Colors.pink,
+        onChanged: (bool val){
+
+        },
+      ),
+    );
+  }
+
+  Widget _cartImage(item){
+    return Container(
+      width: ScreenUtil().setWidth(150),
+      padding: EdgeInsets.all(3.0),
+      decoration: BoxDecoration(
+        border: Border.all(width:1, color:Colors.black12)
+      ),
+      child: Image.network(item.image),
+    );
+  }
+
+  Widget _cartGoodsName(item){
+    return Container(
+      width: ScreenUtil().setWidth(300),
+      padding: EdgeInsets.all(10),
+      alignment: Alignment.topLeft,
+      child: Column(
+        children: <Widget>[
+          Text(item.goodsName)
+        ],
+      ),
+    );
+  }
+
+  Widget _cartPrice(item){
+    return Container(
+      width: ScreenUtil().setWidth(150),
+      alignment: Alignment.centerRight,
+      child: Column(
+        children: <Widget>[
+          Text('￥${item.price}'),
+          Container(
+            child: InkWell(
+              onTap: (){},
+              child: Icon(
+                Icons.delete_forever,
+                color: Colors.black26,
+                size: 30,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
